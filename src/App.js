31992859463435f11e01e298079e9d5a083069be
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-10-19 18:21:01 +0300

    feat: start design development (lesson 11)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
+import logo from './assets/img/avatar.jpg';
 
 const App = () => {
   return (
     <div className='App'>
-      <ul>
-        social network
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
+      <header className='header'>
+        <img src={logo} alt='logo' />
+      </header>
+      <nav className='nav'>
+        <ul className='nav-list'>
+          <li className='nav-list__item'>
+            <a href='#'>Profile</a>
+          </li>
+          <li className='nav-list__item'>
+            <a href='#'>Messages</a>
+          </li>
+          <li className='nav-list__item'>
+            <a href='#'>Profile</a>{' '}
+          </li>
+        </ul>
+      </nav>
+      <main className='content'>content</main>
+      <footer className='footer'>footer</footer>
     </div>
   );
 };
