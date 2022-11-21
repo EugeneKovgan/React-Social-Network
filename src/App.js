1f3844884d8ba8e43c48a117076d37dae8c19547
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-11-21 20:25:30 +0300

    feat: cookie, login (lesson 61)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "normalize.css";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
 import MainBlock from "./components/MainBlock/MainBlock";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <MainBlock />
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <HeaderContainer/>
+                <MainBlock/>
+                <Footer/>
+            </div>
+        </BrowserRouter>
+    );
 };
 
 export default App;
